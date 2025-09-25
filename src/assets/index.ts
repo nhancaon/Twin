79a729584a6b5fd0031a5ca163c82fcdc439a507
nhancaon <caonhan.work@gmail.com>
2025-09-25 15:50:54 +0700

    optimize

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,41 +1,27 @@
-import giz from './giz.jpg';
-import aped from './aped.png';
-import bbu from './bbu.png';
 import agri from './agri.jpg';
 import wood from './wood.jpeg';
 import silk from './silk.jfif';
 import favicon from './favicon.png'
-import dz from './dz.jpg'
 import txt from './txt.png'
 import bg from './bg.png'
-import twin from './twin.png'
 import poster from './poster.png'
 import logo from './logo.jpg'
 import hero_bg from './hero_bg.png'
-import gd from './g&d.png'
-import e1 from './e1.png'
 import fb from './fb.png'
 import qr_agri from './qr_agri.png'
 import qr_silk from './qr_silk.png'
 import qr_wood from './qr_wood.png'
 
 const images = {
-  giz,
-  aped,
-  bbu,
   agri,
   wood,
   silk,
   favicon,
-  dz,
   txt,
   bg,
-  twin,
   poster,
   logo,
   hero_bg,
-  gd,
-  e1,
   fb,
   qr_agri,
   qr_silk,
